8d4ab938c2a07471835e998859e46186bae4acd6
Song Peng <peng.qd@hotmail.com>
2013-12-07 08:54:15 +0800

    Fixed the issue with the back button in browser.

--- scripts/controllers/rootController.js
+++ scripts/controllers/rootController.js
@@ -5,8 +5,11 @@ define(['./module'], function(controllers) {
 		function($scope, $location) {
 			// hide out the sublayouts out of ng-view
 	        $scope.$on('$locationChangeStart', function(event, newVal, oldVal) {
-	        	console.log('URL will be changed to: ' + $location.path());
-	            if($location.path() != "/") {
+	        	if($location.url() == "") return;
+
+	        	var pathTo = newVal.substring(newVal.indexOf('#') + 1);
+	        	console.log('URL will be changed to the: ' + pathTo);
+	            if(pathTo != '/') {
 	                $scope.showHomeComponent = false;
 	            } else {
 	                $scope.showHomeComponent = true;
