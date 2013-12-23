9843a3e02b9c78d65ac32c16a080a9f6be1f2ec5
Song Peng <peng.qd@hotmail.com>
2013-12-23 20:49:13 +0800

    Combined the error message into the custom directive.

--- scripts/directives/validate-not-allowed-characters.js
+++ scripts/directives/validate-not-allowed-characters.js
@@ -4,18 +4,19 @@ define(['./module'], function (directives) {
         return {
             restrict: 'A',
             require: 'ngModel',
-            link: function(scope, elm, attrs, model) {
+            link: function(scope, element, attrs, model) {
                 var notAllowedCharacters = ["<",">","{","}","(",")","[","]","'","\""];
 
                 function validate() {
                     var newValue = model.$viewValue;
-                    console.log(newValue);
 
                     for(var i = 0; i < notAllowedCharacters.length; i++) {
                         if(newValue.indexOf(notAllowedCharacters[i]) === -1) {
                             model.$setValidity('notAllowedCharacters', true);
+                            $(element).siblings('.not-allowed-message').remove();
                         } else {
                             model.$setValidity('notAllowedCharacters', false);   
+                            $(element).after("<span class='not-allowed-message'>The text fields should not allow braces or quotes &lt;&gt;(){}[]&quot'</span>");
                             break;              
                         }
                     }
