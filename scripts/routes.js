43ecde75d7757543beec938b789cbf9e1e27ebc0
peng-qd <peng.qd@hotmail.com>
2014-02-21 11:03:24 +0800

    move the home page view into the index html.

--- scripts/routes.js
+++ scripts/routes.js
@@ -2,7 +2,7 @@ define(['app'], function(app) {
 	'use strict';
 	return app.config(['$routeProvider', function($routeProvider) {
 		$routeProvider.
-			when('/', {templateUrl: '../views/home.html?v='+window.mySettings.buildNumber, controller: 'HomeCtrll'}).
+			//when('/', {templateUrl: '../views/home.html?v='+window.mySettings.buildNumber, controller: 'HomeCtrll'}).
 			when('/report', {templateUrl: '../views/form.html?v='+window.mySettings.buildNumber, controller: 'FormCtrll'}).
 			otherwise({redirectTo: '/'});
 	}]);
